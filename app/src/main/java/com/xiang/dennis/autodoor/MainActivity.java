d0eb00259992accba9e2595cbd0bca2b22cbd0dc
Dennis Xiang <xiangdennis@gmail.com>
2016-07-02 14:01:17 -0700

    Added various functions

--- app/src/main/java/com/xiang/dennis/autodoor/MainActivity.java
+++ app/src/main/java/com/xiang/dennis/autodoor/MainActivity.java
@@ -1,21 +1,53 @@
 package com.xiang.dennis.autodoor;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.xiang.dennis.autodoor.MESSAGE";
+
+    private Button unlock_button = null;
+    private EditText input_passcode = null;
+
+    private BluetoothGattCharacteristic characteristicTx = null;
+    private RBLService mBluetoothLeService;
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothDevice mDevice = null;
+    private String mDeviceAddress;
+
+    private boolean flag = true;
+    private boolean connState = false;
+    private boolean scanFlag = false;   // flag for if device is found, false = not found
+
+    private static final long SCAN_PERIOD = 2000;
+    final private static char[] hexArray = { '0', '1', '2', '3', '4', '5', '6',
+            '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,33 +56,119 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button unlock_button = (Button) findViewById(R.id.btn_unlock);
+
+        unlock_button = (Button) findViewById(R.id.btn_unlock);
         unlock_button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                EditText input_passcode = (EditText) findViewById(R.id.txt_passcode);
-                int passcode = Integer.parseInt(input_passcode.getText().toString());
-                TextView status_text = (TextView) findViewById(R.id.txtview_lock_status);
-                if(passcode == 1234) {
-                    status_text.setText(R.string.unlocked_message);
-                }
-                else {
-                    status_text.setText(R.string.wrong_passcode);
+
+                if (scanFlag == false) {
+                    scanLeDevice();
+
+                    Timer mTimer = new Timer();
+                    mTimer.schedule(new TimerTask() {
+
+                        @Override
+                        public void run() {
+                            if (mDevice != null) {
+                                mDeviceAddress = mDevice.getAddress();
+                                mBluetoothLeService.connect(mDeviceAddress);
+                                scanFlag = true;
+                            }
+                            else {
+                                runOnUiThread(new Runnable() {
+                                    public void run() {
+                                        Toast toast = Toast
+                                                .makeText(
+                                                        MainActivity.this,
+                                                        "Couldn't find BLE shield!",
+                                                        Toast.LENGTH_SHORT);
+                                        toast.setGravity(0, 0, Gravity.CENTER);
+                                        toast.show();
+                                    }
+                                });
+                            }
+                        }
+                    }, SCAN_PERIOD);
                 }
 
+                System.out.println(connState);
+                if (connState == false) {
+                    mBluetoothLeService.connect(mDeviceAddress);
+                }
+                else {
+                    mBluetoothLeService.disconnect();
+                    mBluetoothLeService.close();
+                    setButtonDisable();
+                }
+
+                if (!getPackageManager().hasSystemFeature( PackageManager.FEATURE_BLUETOOTH_LE)) {
+                    Toast.makeText(this, "Ble not supported", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+
+                final BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+                mBluetoothAdapter = mBluetoothManager.getAdapter();
+                if (mBluetoothAdapter == null) {
+                    Toast.makeText(this, "BLE not supported", Toast.LENGTH_SHORT)
+                            .show();
+                    finish();
+                    return;
+                }
+
+                Intent gattServiceIntent = new Intent(MainActivity.this,
+                        RBLService.class);
+                bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
             }
         });
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
+
     }
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName,
+                                       IBinder service) {
+            mBluetoothLeService = ((RBLService.LocalBinder) service)
+                    .getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+
+            if (RBLService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                Toast.makeText(getApplicationContext(), "Disconnected",
+                        Toast.LENGTH_SHORT).show();
+                setButtonDisable();
+            } else if (RBLService.ACTION_GATT_SERVICES_DISCOVERED
+                    .equals(action)) {
+                Toast.makeText(getApplicationContext(), "Connected",
+                        Toast.LENGTH_SHORT).show();
+
+                getGattService(mBluetoothLeService.getSupportedGattService());
+            } else if (RBLService.ACTION_DATA_AVAILABLE.equals(action)) {
+                data = intent.getByteArrayExtra(RBLService.EXTRA_DATA);
+
+                readAnalogInValue(data);
+            } else if (RBLService.ACTION_GATT_RSSI.equals(action)) {
+                displayData(intent.getStringExtra(RBLService.EXTRA_DATA));
+            }
+        }
+    };
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,14 +192,88 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /* Called when the user clicks the Send button */
-    /*
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+    private void scanLeDevice() {
+        new Thread() {
+
+            @Override
+            public void run() {
+                mBluetoothAdapter.startLeScan(mLeScanCallback);
+
+                try {
+                    Thread.sleep(SCAN_PERIOD);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            }
+        }.start();
     }
-    */
+
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi,
+                             final byte[] scanRecord) {
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    byte[] serviceUuidBytes = new byte[16];
+                    String serviceUuid = "";
+                    for (int i = 32, j = 0; i >= 17; i--, j++) {
+                        serviceUuidBytes[j] = scanRecord[i];
+                    }
+                    serviceUuid = bytesToHex(serviceUuidBytes);
+                    if (stringToUuidString(serviceUuid).equals(
+                            RBLGattAttributes.BLE_SHIELD_SERVICE
+                                    .toUpperCase(Locale.ENGLISH))) {
+                        mDevice = device;
+                    }
+                }
+            });
+        }
+    };
+
+    private String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        int v;
+        for (int j = 0; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
+    private String stringToUuidString(String uuid) {
+        StringBuffer newString = new StringBuffer();
+        newString.append(uuid.toUpperCase(Locale.ENGLISH).substring(0, 8));
+        newString.append("-");
+        newString.append(uuid.toUpperCase(Locale.ENGLISH).substring(8, 12));
+        newString.append("-");
+        newString.append(uuid.toUpperCase(Locale.ENGLISH).substring(12, 16));
+        newString.append("-");
+        newString.append(uuid.toUpperCase(Locale.ENGLISH).substring(16, 20));
+        newString.append("-");
+        newString.append(uuid.toUpperCase(Locale.ENGLISH).substring(20, 32));
+
+        return newString.toString();
+    }
+
+    // Enables the unlock button
+    private void setButtonEnable() {
+        flag = true;
+        connState = true;
+        unlock_button.setEnabled(flag);
+    }
+
+    // Disables the unlock button
+    private void setButtonDisable() {
+        flag = false;
+        connState = false;
+        unlock_button.setEnabled(flag);
+    }
+
 }
